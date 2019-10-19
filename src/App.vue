0e7eba81c786ad818ab6f8863a645110ee306fc5
sqsyc <i_shenqing@163.com>
2019-10-19 19:11:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(user) in users" :key="user.id">
+        {{user.name}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'app',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then(res=>{
+      this.users = res.data;
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
